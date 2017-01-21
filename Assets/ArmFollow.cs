d0a355eb7e82cd58f6160f2f6090307bcee28db4
gregolopogus <gregoryperry627@gmail.com>
2017-01-20 22:22:43 -0800

    Added arm follower
    Added a "Gum Aimer" prefab that follows the mouse

---
+++ Assets/ArmFollow.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmFollow : MonoBehaviour {
+
+  
+    private Vector3 armVector;
+    private Vector3 gunAimerPos;
+    private Vector3 mousePosition;
+    private float angle;
+
+   
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        /*
+        Vector3 zeroAngle;
+        zeroAngle.x = 1; zeroAngle.y = 0; zeroAngle.z = 0;
+        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePosition.z = 0;
+        float mouseAngle = Vector3.Angle(zeroAngle, mousePosition);
+        print("" + mouseAngle);
+        if (transform.position.y > mousePosition.y)
+        {
+            transform.rotation = Quaternion.Euler(0, 0, -mouseAngle);
+        }
+        else
+        {
+            transform.rotation = Quaternion.Euler(0, 0, mouseAngle);
+        }*/
+
+        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePosition.z = 0;
+        Vector3 direction = mousePosition - transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x)*180/Mathf.PI;
+        transform.rotation = Quaternion.Euler(0, 0, angle);
+
+
+    }
+}
