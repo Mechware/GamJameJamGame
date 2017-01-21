32b6abb9edb257d2a12ab4419a1d903da26715c5
Michael Demone <m_d_emone@hotmail.com>
2017-01-20 21:41:07 -0800

    First Commit!
    Added a player that can shoot and can move.
    Also added a bullet that the player can shoot, it moves in a wavey
    pattern

---
+++ Assets/Weapon/ShootBullets.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootBullets : MonoBehaviour {
+
+    public GameObject bullet;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonDown(0)) {
+            Vector3 mouse = Input.mousePosition;
+            mouse.z = 0;
+            Vector3 mouseWorldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mouseWorldPosition.z = 0;
+            Vector3 direction = transform.position - mouseWorldPosition;
+            GameObject bulletObj = Instantiate(bullet);
+            bulletObj.GetComponent<Bullet>().setStartPosition(transform.position, direction);
+
+        }
+	}
+}
