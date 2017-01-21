c84dfe942cd6ddc505a66e7f2e0ba5e923b9adad
twistedburger <twistedburger@hotmail.com>
2017-01-21 11:42:00 -0800

    TWO PLAYERS
    TWO PLAYERS YAAAAAAY

--- Assets/ArmFollow.cs
+++ Assets/ArmFollow.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class ArmFollow : MonoBehaviour {
 
-    [SerializeField] private string joyX;
-    [SerializeField] private string joyY;
+    [SerializeField] private int playerNumber;
+    //[SerializeField] private string joyY;
     private Vector3 armVector;
     private Vector3 gunAimerPos;
     private Vector3 mousePosition;
@@ -23,8 +23,7 @@ public class ArmFollow : MonoBehaviour {
       
         mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         mousePosition.z = 0;
-        Vector3 direction = new Vector3(Input.GetAxis(joyX), Input.GetAxis(joyY), 0); 
-        print(direction);
+        Vector3 direction = new Vector3(Input.GetAxis("Joy X " + playerNumber), Input.GetAxis("Joy Y " + playerNumber), 0); 
         float angle = Mathf.Atan2(direction.y, direction.x)*180/Mathf.PI;
         float parentRotation = transform.parent.localScale.x > 0 ? 0 : 1;
         transform.rotation = Quaternion.Euler(0, 0, angle + 180*parentRotation);
--- Assets/Weapon/ShootBullets.cs
+++ Assets/Weapon/ShootBullets.cs
@@ -6,9 +6,9 @@ public class ShootBullets : MonoBehaviour {
 
     public GameObject bullet;
     public Transform startPosition;
-    [SerializeField] private string fire;
-    [SerializeField] private string joyX;
-    [SerializeField] private string joyY;
+    [SerializeField] private int playerNumber;
+    //[SerializeField] private string joyX;
+    //[SerializeField] private string joyY;
     
 
 
@@ -19,10 +19,11 @@ public class ShootBullets : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if(Input.GetMouseButtonDown(0) || Input.GetButtonDown(fire)) {
+		if(Input.GetButtonDown("Fire " + playerNumber)) {
+            print(playerNumber);
             Vector3 mouse = Input.mousePosition;
             mouse.z = 0;            
-            Vector3 joystickPosition = new Vector3(Input.GetAxis(joyX), Input.GetAxis(joyY), 0);
+            Vector3 joystickPosition = new Vector3(Input.GetAxis("Joy X " + playerNumber), Input.GetAxis("Joy Y " + playerNumber), 0);
             Vector3 direction = joystickPosition;
             GameObject bulletObj = Instantiate(bullet);
             bulletObj.GetComponent<Bullet>().setStartPosition(startPosition.position, direction);
