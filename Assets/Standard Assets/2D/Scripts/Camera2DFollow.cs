8d649b3fa0060ebb8de8910dfa60bd14fc59b1de
Michael Demone <m_d_emone@hotmail.com>
2017-01-21 21:23:31 -0800

    Added more players!

--- Assets/Standard Assets/2D/Scripts/Camera2DFollow.cs
+++ Assets/Standard Assets/2D/Scripts/Camera2DFollow.cs
@@ -9,7 +9,7 @@ public class Camera2DFollow : MonoBehaviour
     //public float lookAheadFactor = 3;
     // public float lookAheadReturnSpeed = 0.5f;
     // public float lookAheadMoveThreshold = 0.1f;
-    public string p1, p2, p3, p4;
+    public GameObject[] players;
 
     // private float m_OffsetZ;
     // private Vector3 m_LastTargetPosition;
@@ -22,7 +22,7 @@ public class Camera2DFollow : MonoBehaviour
     private float maxZoom = 43;
     private float minZoom = 15;
 
-    private List<GameObject> players;
+    private List<GameObject> _players;
     private float startZ;
 
     // Use this for initialization
@@ -30,21 +30,6 @@ public class Camera2DFollow : MonoBehaviour
     {
         cam = GetComponent<Camera>();
         startZ = cam.transform.position.z;
-
-        players = new List<GameObject>();
-        GameObject player = GameObject.Find(p1);
-        if(player != null)
-            players.Add(GameObject.Find(p1));
-        player = GameObject.Find(p2);
-        if (player != null)
-            players.Add(GameObject.Find(p2));
-        player = GameObject.Find(p3);
-        if (player != null)
-            players.Add(GameObject.Find(p3));
-        player = GameObject.Find(p4);
-        if (player != null)
-            players.Add(GameObject.Find(p4));
-        
     }
 
 
@@ -52,34 +37,37 @@ public class Camera2DFollow : MonoBehaviour
     private void Update()
     {
 
-        Vector3 playerA = players[0].transform.position;
-        Vector3 PlayerB = players[0].transform.position;
+        float playerAx = players[0].transform.position.x;
+        float playerBx = players[0].transform.position.y;
+        float playerCy = players[0].transform.position.x;
+        float playerDy = players[0].transform.position.y;
         Vector3 maxDistance = new Vector3(0, 0, 0);
         foreach(GameObject player in players) {
             foreach(GameObject player2 in players) {
                 Vector3 difference = (player2.transform.position - player.transform.position);
-                if (difference.magnitude > maxDistance.magnitude) {
-                    playerA = player.transform.position;
-                    PlayerB = player2.transform.position;
-                    maxDistance = difference;
+                if (difference.x > maxDistance.x) {
+                    playerAx = player.transform.position.x;
+                    playerBx = player2.transform.position.x;
+                    maxDistance.x = difference.x;
+                }
+                if(difference.y > maxDistance.y) {
+                    playerCy = player.transform.position.y;
+                    playerDy = player2.transform.position.y;
+                    maxDistance.y = difference.y;
                 }
             }
         }
 
-        Vector3 inTheMiddle = (playerA + PlayerB)/2;
+        Vector3 inTheMiddle = new Vector3((playerAx + playerBx)/2, (playerCy + playerDy)/2);
         inTheMiddle.z = -10;
-        transform.position = inTheMiddle;
-        print(maxDistance.magnitude);
+        transform.position = Vector3.Lerp(transform.position, inTheMiddle, Time.deltaTime);
 
         if(maxDistance.magnitude < minLength) {
-            cam.orthographicSize = minZoom;
+            cam.orthographicSize = minZoom+5;
         } else if (maxDistance.magnitude > maxLength) {
-            cam.orthographicSize = maxZoom;
+            cam.orthographicSize = maxZoom+5;
         } else {
-            cam.orthographicSize = ((maxDistance.magnitude-minLength)/(maxLength-minLength))*(maxZoom-minZoom)+minZoom;
+            cam.orthographicSize = ((maxDistance.magnitude-minLength)/(maxLength-minLength))*(maxZoom-minZoom)+minZoom+5;
         }
-
-        return;
-        
     }
 }
